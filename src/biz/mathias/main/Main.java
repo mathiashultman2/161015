ea135dffa6fc6fa30281e5c05296a99c7c8deec0
Mathias Hultman <hultman@mensa.se>
2016-10-15 15:31:28 +0200

    added go method

--- src/biz/mathias/main/Main.java
+++ src/biz/mathias/main/Main.java
@@ -3,5 +3,9 @@ package biz.mathias.main;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("check");
+		new Main().go();
+	}
+	public void go(){
+		System.out.println("in go");
 	}
 }
